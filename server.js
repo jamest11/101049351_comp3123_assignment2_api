8416f1346230c899eec44ba66aae491ac49b4aa6
jamest11 <jameswtory@gmail.com>
2022-11-13 17:05:36 -0500

    Config .env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-require('dotenv').config()
+// require('dotenv').config()
 
 const employeeRoutes = require('./routes/employee_routes');
 const userRoutes = require('./routes/user_routes');
