cd8df3d4826c0255571d456fc3914f2a188fee87
jamest11 <jameswtory@gmail.com>
2022-11-15 13:13:25 -0500

    CORS

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ mongoose.connect(process.env.DB_URI, {
     process.exit();
 });
 
-//app.use(cors({
-//    origin: 'https://comp3123-a2-api-43405.herokuapp.com'
-//}));
+app.use(cors({
+    origin: ['https://comp3123-a2-api-43405.herokuapp.com/', 'http://localhost:3000/']
+}));
 
 app.use('/api/emp', employeeRoutes);
 app.use('/api/user', userRoutes);
